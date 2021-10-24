d42752edf978d101953ca0a5f7126a9e7bb30086
Jozias Martini <jozias.martini@compasso.com.br>
2021-10-23 21:14:40 -0300

    New project version, refactoring dhammapada

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-var pastas = ["arquivo_textos_theravada", "caminho_liberdade", "dhp", "sutta"]
+var cache = [];
 
 $(function(){
+    loadCache();
     loadContent("layout/nav.html", "#nav-placeholder");
     loadContent("layout/footer.html", "#footer-placeholder");
     if(!getPageFromUrl())
@@ -21,7 +22,7 @@ function getPageFromUrl(){
     return true
 }
 
-function loadContent(pageRelativePath, target = '#body-placeholder', profundidade = 0){
+function loadContent(pageRelativePath, target = '#body-placeholder'){
     $.ajaxSetup({
         'beforeSend' : function(xhr) {
             xhr.overrideMimeType('text/html; charset=ISO-8859-1');
@@ -29,26 +30,55 @@ function loadContent(pageRelativePath, target = '#body-placeholder', profundidad
         cache: false
     });
 
-    $(target).load(pageRelativePath, function( response, status, xhr ) {
-        const partes = pageRelativePath.split("/");
-        const arquivo = partes[partes.length-1];
+    const pageFromCache = getPathFromCache(pageRelativePath);
+    loadContenteHandler(pageFromCache, target);
+}
+
+function getPathFromCache(pageRelativePath){
+    const filename = getFilenameFromPath(pageRelativePath);
+    const dir = cache[filename];
+    if(dir === undefined){
+        console.warn(`Arquivo ${filename} nao foi encontrado no cache!`)
+        return pageRelativePath;
+    }    
+    return dir ? `${dir}/${filename}` : filename;
+}
+
+function loadContenteHandler(pageRelativePath, target){
+    $(target).load(pageRelativePath, function(resp, status, xhr) {
+        const filename = getFilenameFromPath(pageRelativePath);
         if ( status == "error" ) {
-            if(profundidade > pastas.length){
-                var msg = "Não foi possível carregar o arquivo: " + pageRelativePath;
-                alert( msg + " " + xhr.status + " " + xhr.statusText );
-            }
-            else {
-                const newPageRelativePath = pastas[profundidade] + "/" + arquivo;
-                loadContent(newPageRelativePath, target, ++profundidade)
-            }
+            var msg = "Não foi possível carregar o arquivo: " + pageRelativePath;
+            alert( msg + " " + xhr.status + " " + xhr.statusText );
         }
         else {
-            const protocol = window.location.protocol;
-            const hostname = window.location.hostname;
-            const port = window.location.port;
-            const pathName = window.location.pathname;
-            const url = `${protocol}//${hostname}:${port}${pathName}?page=` + arquivo
-            window.history.pushState("", "", url);
+            addFilenameToUrl(filename);
+        }
+    });
+}
+
+function addFilenameToUrl(filename){
+    const protocol = window.location.protocol;
+    const hostname = window.location.hostname;
+    const port = window.location.port;
+    const pathName = window.location.pathname;
+    const url = `${protocol}//${hostname}:${port}${pathName}?page=` + filename
+    window.history.pushState("", "", url);
+}
+
+function getFilenameFromPath(pageRelativePath){
+    const partes = pageRelativePath.split("/");
+    return partes[partes.length-1];
+}
+
+function loadCache(){
+    $.ajax({
+        url: "_getcache.php", 
+        success: function(resp){
+            cache = resp
+        },
+        error(xhr){
+            console.error(xhr.responseText)
         }
     });
 }
